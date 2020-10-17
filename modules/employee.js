f1eef8c7daac0b1f61a753eae5cb9e98b6df394d
Chad Smith <39306798+ChadESmith42@users.noreply.github.com>
2020-10-17 14:19:20 -0500

    add employee class

---
+++ modules/employee.js
@@ -0,0 +1,10 @@
+
+export default class Employee {
+    constructor(firstName, lastName, id, title, annualSalary) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Id = id;
+        this.Title = title;
+        this.AnnualSalary = annualSalary;
+    }
+}
